18baf8d46dba4a7a66e939997eac531c6615fd1e
leoraggy <leoragual35@gmail.com>
2026-04-09 10:41:59 -0700

    Added styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import "./App.css";
 import FunFactCard from "./FunFactCard";
 function App() {
   return (
-    <div>
-      <FunFactCard fact="I like spicy food." />
-      <FunFactCard fact="I am Filipino." />
-      <FunFactCard fact="I like to play Tekken 8." />
-      <FunFactCard fact="I have visited California." />
-      <FunFactCard fact="I like to play Minecraft." />
-    </div>
+    <>
+      <h1>Facts App</h1>
+      <div className="container">
+        <div className="fact">
+          <h2>Leo's Facts</h2>
+          <FunFactCard fact="I am Filipino." />
+          <FunFactCard fact="I like to play Tekken 8." />
+          <FunFactCard fact="I like spaghetti." />
+        </div>
+        <div className="fact">
+          <h2>Darien's Facts</h2>
+          <FunFactCard fact="I like spicy food." />
+          <FunFactCard fact="I have visited California." />
+          <FunFactCard fact="I like to play Minecraft." />
+        </div>
+      </div>
+    </>
   );
 }
 
